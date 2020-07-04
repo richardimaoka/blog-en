ea0832a945a62ac1c84b7914dfecc79887e7460f
Richard Imaoka <richard.e.imaoka@gmail.com>
2020-07-05 05:41:53 +0900

    accomodate to vercel

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const isProduction = process.env.NODE_ENV === "production"
-
-module.exports = {
-  assetPrefix: isProduction ? '/bloga' : '',
-  publicRuntimeConfig: {
-    basePath: isProduction ? '/bloga' : ''
-  }
-}
