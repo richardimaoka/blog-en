0c6f032da9dee2464e45b042dbcbe2db47d3afd3
Richard Imaoka <richard.e.imaoka@gmail.com>
2020-07-21 21:11:11 +0900

    fix meta tags

--- pages/[id].js
+++ pages/[id].js
@@ -10,7 +10,19 @@ export default function Post({ postData }) {
     <Layout>
       <Head>
         <title>{postData.title}</title>
+        <meta name="og:title" content={postData.title} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@richardimaoka" />
+        <meta name="twitter:creator" content="@richardimaoka" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/styles/agate.min.css"></link>
+        { postData.description &&
+          <meta name="og:description" content={postData.description} />
+        }
+        { postData.image &&
+          <meta name="og:image" content={"https://blog-jp.richardimaoka.net" + postData.image} />
+        }
+        <script src="//cdn.iframe.ly/embed.js" charset="utf-8"></script>
+        <script src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
